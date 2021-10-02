2fcf8a9b004518d3720ea86956817ef7af1cdf16
IsaacDynamo <61521674+IsaacDynamo@users.noreply.github.com>
2021-10-02 20:27:33 +0200

    typo

--- os/common/ext/SN/SN32F26x/SN32F240B.h
+++ os/common/ext/SN/SN32F26x/SN32F240B.h
@@ -1,2 +1,2 @@
-// This file exists are a workaround to get the 240B USB code building on a 260.
+// This file exists as a workaround to get the 240B USB code building on a 260.
 #include "SN32F260.h"
